8339c56df7206d7ad27ea606d86ca086bcde7c66
Corbin Fraser <corbin@commandbase.ca>
2017-12-03 20:50:52 +0900

    loading animation

--- js/txhighway.js
+++ js/txhighway.js
@@ -621,3 +621,24 @@ $('.close').on('click', function(){
 window.onclick = function(event) {
 	if($(event.target).hasClass('modal')) $('.modal').hide();
 }
+
+
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 1500);
+
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('page', true);
+    show('loading', false);
+});
