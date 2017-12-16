cd8206caf0d22bd150792dbe0962424abdfc8e18
PorlyBe <porlybe@gmail.com>
2017-12-17 06:46:00 +1100

    Fixed speed burst on sw lane when block is found. Added better mobile detection.

--- js/txhighway.js
+++ js/txhighway.js
@@ -80,7 +80,7 @@ let WIDTH = canvas.width;
 let HEIGHT = canvas.height;
 let SINGLE_LANE = HEIGHT/14;
 let SPEED = 8;
-let SPEED_MODIFIER = 0.5;
+let SPEED_MODIFIER = 0;
 let VOLUME = 1;
 let PRICE_BCH = 0;
 let PRICE_BTC = 0;
@@ -122,7 +122,7 @@ socketCash.onmessage = (onmsg) =>{
 
 	if (res.op == "utx"){
 		let t = parseInt(cashPoolInfo.textContent.replace(/\,/g,''));			
-		cashPoolInfo.textContent = formatNumbersWithCommas(t +1);
+		cashPoolInfo.textContent = formatWithCommas(t +1);
 
 		newTX(true, res.x);
 	} else {
@@ -135,7 +135,7 @@ socketCore.onmessage = (onmsg)=> {
 
 	if (res.op == "utx"){
 		let t = parseInt(corePoolInfo.textContent.replace(/\,/g,''));			
-		corePoolInfo.textContent = formatNumbersWithCommas(t +1);
+		corePoolInfo.textContent = formatWithCommas(t +1);
 
 		res.x.inputs.forEach(i => {
             if (JSON.stringify(i.script).length < 120){
@@ -184,7 +184,7 @@ function init(){
 	carSegwit.src = "assets/sprites/segwit.png";
 
 	// hide signes on small screens
-	if(canvas.width <= 800 && canvas.height <= 640) {
+	if(window.mobilecheck) {
 		$("input.overlay-switch")[0].checked = true;
 		$( ".sign" ).fadeToggle( "slow", "linear" );
 	}
@@ -217,6 +217,12 @@ function init(){
 	requestID = requestAnimationFrame(animate);
 }
 
+window.mobilecheck = function(){
+	let check = false;
+	(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+	return check;
+}
+
 function getPriceData(url){
 	let xhr = new XMLHttpRequest();
 
@@ -225,10 +231,10 @@ function getPriceData(url){
 			let res = JSON.parse(this.responseText);
 			if (res[0].symbol == "BCH"){
 				PRICE_BCH = res[0].price_usd;
-				document.getElementById("price_bch").textContent = "USD $" + formatNumbersWithCommas(PRICE_BCH);
+				document.getElementById("price_bch").textContent = "USD $" + formatWithCommas(parseFloat(PRICE_BCH).toFixed(2));
 			} else {
 				PRICE_BTC = res[0].price_usd;
-				document.getElementById("price_btc").textContent = "USD $" + formatNumbersWithCommas(PRICE_BTC);
+				document.getElementById("price_btc").textContent = "USD $" + formatWithCommas(parseFloat(PRICE_BTC).toFixed(2));
 			}
 		}
 	}
@@ -238,7 +244,7 @@ function getPriceData(url){
 }
 
 // adds thousands seperator to large numbers
-function formatNumbersWithCommas(x){
+function formatWithCommas(x){
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");	
 }
 
@@ -252,12 +258,13 @@ function blockNotify(data, isCash){
 		ticker = "BCH";
 		t = parseInt(cashPoolInfo.textContent.replace(/\,/g,''));
 		amount = data.nTx;
-		cashPoolInfo.textContent = formatNumbersWithCommas(t - amount);//"UPDATING";
+		cashPoolInfo.textContent = formatWithCommas(t - amount);//"UPDATING";
 	} else {
 		ticker = "BTC";
 		t = parseInt(corePoolInfo.textContent.replace(/\,/g,''));
 		amount = data.nTx;
 
+		
 		// sets speed modifier for btc lane
 		let mod = t/amount/100;
 		if (mod >= 0.9){
@@ -266,7 +273,7 @@ function blockNotify(data, isCash){
 			SPEED_MODIFIER = 1 - mod;
 		}
 
-		corePoolInfo.textContent = formatNumbersWithCommas(t - amount);
+		corePoolInfo.textContent = formatWithCommas(t - amount);
 	}
 
 	if (isVisible) playSound(audioChaChing);
@@ -291,14 +298,16 @@ function getPoolData(url, xhr, isCash){
 			obj.data.forEach((key)=>{
 				if (key.e =="mempool_transactions"){
 					if (isCash){
-						cashPoolInfo.textContent = formatNumbersWithCommas(key.c);
+						cashPoolInfo.textContent = formatWithCommas(key.c);
 					} else {
-						corePoolInfo.textContent = formatNumbersWithCommas(key.c);
-						let mod = key.c/2400/100;
-						if (mod >= 0.9){
-							SPEED_MODIFIER = 0.9;
-						} else {
-							SPEED_MODIFIER = 1 - mod;
+						corePoolInfo.textContent = formatWithCommas(key.c);
+						if (SPEED_MODIFIER == 0){
+							let mod = key.c/2400/100;
+							if (mod >= 0.9){
+								SPEED_MODIFIER = 0.9;
+							} else {
+								SPEED_MODIFIER = 1 - mod;
+							}
 						}
 					}
 				}
